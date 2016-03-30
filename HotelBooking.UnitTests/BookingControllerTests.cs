558e597a0b1bbf3892c586b7ea7312c0ac862679
Adi Tanase <Adi Tanase>
2016-03-30 13:00:37 +0200

    few adds in the booking controler

--- HotelBooking.UnitTests/BookingControllerTests.cs
+++ HotelBooking.UnitTests/BookingControllerTests.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using HotelBooking.DAL.Repositories;
+using HotelBooking.Models;
+using HotelBooking.Controllers;
+using NSubstitute;
 
 namespace HotelBooking.UnitTests
 {
     class BookingControllerTests
     {
+       
     }
-}
+    }
